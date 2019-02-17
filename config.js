b9a03e7db2303497fd5653f8d0de4f6c2aafdd31
Nomaan Ahgharian <get@nomaan.me>
2019-02-16 20:39:14 -0600

    initial commit

---
+++ config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  title: 'Blog by Nomaan Ahgharian',
+  subtitle: 'Lazy Developer',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Nomaan Ahgharian',
+    photo: '/nomaan.png',
+    bio: 'Lazy Developer',
+    contacts: {
+      email: 'get@nomaan.me',
+      twitter: 'https://twitter.com/ahgharian',
+      github: 'https://github.com/ahgharian'
+    }
+  }
+};
